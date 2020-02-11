8a211d4e960398e005c14e14b239ca16a4aa3ab6
Artem <krekotnev@gmail.com>
2020-02-11 11:57:31 +0300

    little bit update

--- Assets/Step.cs
+++ Assets/Step.cs
@@ -8,8 +8,5 @@ public class Step : MonoBehaviour
     [TextArea(minLines: 10, maxLines: 30)] public string content = "Я приветствую тебя в квесте ";
     public Step[] nextStep;
     public Sprite Image;
-    public Font font;
     
-
-
 }
