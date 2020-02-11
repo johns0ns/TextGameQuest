bd1ca38ee26fd8b8187bf4160ee067d5fcfece33
Artem <krekotnev@gmail.com>
2020-02-11 13:45:42 +0300

    update 2

--- Assets/TextGameQuest.cs
+++ Assets/TextGameQuest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class TextGameQuest : MonoBehaviour
 {
     [Header("Элементы")]
@@ -16,6 +17,7 @@ public class TextGameQuest : MonoBehaviour
 
     void Start()
     {
+
         titleText.text = title;
         gametext.text = activeStep.content;
         
@@ -31,20 +33,20 @@ public class TextGameQuest : MonoBehaviour
             CheckPress(0);
                 gameimage.sprite = activeStep.Image;
         }
-        if ((Input.GetKeyDown(KeyCode.Alpha2)) || (Input.GetKeyDown(KeyCode.Keypad2)))
+        else if ((Input.GetKeyDown(KeyCode.Alpha2)) || (Input.GetKeyDown(KeyCode.Keypad2)))
         {
             CheckPress(1);
                 gameimage.sprite = activeStep.Image;
         }
 
-        if ((Input.GetKeyDown(KeyCode.Alpha3)) || (Input.GetKeyDown(KeyCode.Keypad3)))
+        else if ((Input.GetKeyDown(KeyCode.Alpha3)) || (Input.GetKeyDown(KeyCode.Keypad3)))
         {
             CheckPress(2);
             gameimage.sprite = activeStep.Image;
         }
-        if ((Input.GetKey(KeyCode.Q)) && Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.E))
+        else if ((Input.GetKey(KeyCode.Q))  && Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.E))
         {
-            Start();
+            SceneManager.LoadScene(0);
         }
 
     }
